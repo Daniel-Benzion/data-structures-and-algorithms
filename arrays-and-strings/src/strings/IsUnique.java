fa84173f6dbd5999f6deae0e1fb824d0bd4162f7
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-14 10:49:41 -0400

    isUnique

--- arrays-and-strings/src/strings/IsUnique.java
+++ arrays-and-strings/src/strings/IsUnique.java
@@ -14,4 +14,21 @@ public class IsUnique {
 		}
 		return true;
 	}
+	//Time: O(n)
+	//Space: O(n)
+	
+
+
+	//Without using additional data structures?
+	public static boolean solution2(String str) {
+		for (int i = 0; i < str.length() - 1; i++) {
+			for (int j = i + 1; j < str.length(); j++) {
+				if (str.charAt(i) == str.charAt(j)) return false;
+			}
+		}
+		return true;
+	}
+	//Time: O(n^2)
+	//Space: O(1)
+
 }
