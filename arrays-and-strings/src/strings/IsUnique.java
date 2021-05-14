a02f13c90f270ddad350cdc4ecbf498a2bfadbf4
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-14 10:42:02 -0400

    isUnique

--- arrays-and-strings/src/strings/IsUnique.java
+++ arrays-and-strings/src/strings/IsUnique.java
@@ -2,5 +2,16 @@ package strings;
 
 //Implement an algorithm to determine if a string has all unique characters.
 
+import java.util.HashSet;
+
 public class IsUnique {
+
+	public static boolean solution(String str) {
+		HashSet<Character> set = new HashSet<>();
+
+		for (int i = 0; i < str.length(); i++) {
+			if (!set.add(str.charAt(i))) return false;
+		}
+		return true;
+	}
 }
