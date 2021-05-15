c5782b6e204fb02bbcf77d0547cd76a5e3a8fbd8
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-15 14:50:14 -0400

    Singly-linked list implementation

--- linked-list/src/single/Node.java
+++ linked-list/src/single/Node.java
@@ -33,4 +33,20 @@ public class Node {
 		return head;
 	}
 
+	public static void print(Node node) {
+
+		Node current = node;
+		while (current.next != null) {
+			System.out.print("(" + current.data + ")-->");
+			current = current.next;
+		}
+		System.out.print("(" + current.data + ")-->null");
+	}
+
+	public static void main(String[] args) {
+		Node node = new Node(1);
+		node.next = new Node(2);
+		node.next.next = new Node(3);
+		print(node);
+	}
 }
