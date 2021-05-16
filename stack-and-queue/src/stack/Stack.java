5f0afdf09db2d1b8a9efdd671f40c4780d9be14b
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-16 16:56:15 -0400

    Stack implementation

---
+++ stack-and-queue/src/stack/Stack.java
@@ -0,0 +1,60 @@
+package stack;
+
+public class Stack {
+
+	Node top;
+
+	static class Node {
+
+		int data;
+		Node next;
+
+		public Node() {
+
+		}
+
+		public Node(int data) {
+			this.data = data;
+		}
+
+	}
+
+	public Stack() {}
+
+	public Stack(Node top) {
+		this.top = top;
+	}
+
+	public void peek() {
+		if (this.top == null) {
+			System.out.println("Stack is empty");
+			return;
+		}
+		System.out.println(this.top.data);
+	}
+
+	public int pop () {
+		if (this.top == null) {
+			System.out.println("Stack is empty");
+			return -1;
+		}
+		int tempData = this.top.data;
+		if (this.top.next == null) {
+			this.top = null;
+			return tempData;
+		}
+		this.top = this.top.next;
+		return tempData;
+	}
+
+	public int push(Node node) {
+		if (this.top == null) {
+			this.top = node;
+			return node.data;
+		}
+		node.next = this.top;
+		this.top = node;
+		return node.data;
+	}
+	
+}
